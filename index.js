8555239836ac5dfb993eb1f8f5ed0c4cd41813a0
Simar Singh <simar@codejudge.io>
2020-06-22 02:06:11 +0530

    sonar fixes

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 $(function(){
-    
+
     // accessing API_URL present in env.js
     console.log(API_URL);
-    
+
     $('#dynamic-content').text("Dynamic content");
 
-    
-    
-
     // jQuery methods go here...
-  
+
 });
